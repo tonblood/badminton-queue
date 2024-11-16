0ef901484ea9e5dc41b3a419acc8d497e48e06ce
Tonblood <tonapichat@kkumail.com>
2024-11-16 17:50:45 +0700

    fix deploy fail

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-module.exports = {
-    typescript: {
-      ignoreBuildErrors: true, // This will bypass TypeScript checks during build
-    },};
 
 export default nextConfig;
